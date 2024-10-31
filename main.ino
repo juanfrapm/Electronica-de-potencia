4814dd4cb2f671bca8b9b48da1991536b1035a37
Juan Francisco Pirez <135382640+juanfrapm@users.noreply.github.com>
2024-10-31 17:03:48 -0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,16 +1,24 @@
-void IRAM_ATTR interrupcion(){ // funcion que se ejecuta con la interrupción
-  cuenta++;
-}
+//const int pinInt = 0;
+const int prueba = 4;
+
+//void IRAM_ATTR interrupcion(){ // funcion que se ejecuta con la interrupción
+
+//}
 
 void setup() {
   
-  pinMode(pinInt, INPUT_PULLUP); 
-  
-  attachInterrupt(digitalPinToInterrupt(pinInt), interrupcion, RISING);
+  //pinMode(pinInt, INPUT_PULLUP); 
+  pinMode(prueba, OUTPUT);
+  pinMode(2, OUTPUT);
+  //attachInterrupt(digitalPinToInterrupt(pinInt), interrupcion, RISING);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  delay(8000);
+  digitalWrite(2, HIGH);
+  digitalWrite(prueba, HIGH);
+  delay(3000);
+  digitalWrite(prueba, LOW);
+  digitalWrite(2, LOW);
 }
